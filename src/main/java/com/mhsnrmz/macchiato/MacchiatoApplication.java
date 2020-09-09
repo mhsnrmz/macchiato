c700d98bcb6a17f309e19b8313f1a72e3b026d88
Mohsen <mhsnrmz@gmail.com>
2020-09-09 22:07:31 +0430

    init

---
+++ src/main/java/com/mhsnrmz/macchiato/MacchiatoApplication.java
@@ -0,0 +1,13 @@
+package com.mhsnrmz.macchiato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacchiatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacchiatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhsnrmz/macchiato/MacchiatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhsnrmz.macchiato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacchiatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
