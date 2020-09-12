5140f5870f7f776561a31b887785ece77390a12a
Mohsen <mhsnrmz@gmail.com>
2020-09-12 18:49:15 +0430

    add order and table entities

---
+++ src/main/java/com/mhsnrmz/macchiato/repository/entity/Table.java
@@ -0,0 +1,17 @@
+package com.mhsnrmz.macchiato.repository.entity;
+
+import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class Table extends UniversalIdentifiableEntity {
+    private Boolean empty;
+    private Boolean reserved;
+}
